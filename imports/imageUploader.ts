e1bc70bfd99393e7a79b6b5f584a301440e549a3
DBereznak <dbereznak@gmail.com>
2020-11-18 16:49:23 -0500

    added a few more filters

--- imports/imageUploader.ts
+++ imports/imageUploader.ts
@@ -2,7 +2,6 @@ class ImageUploader {
     hostDiv: string;
     hasImage: boolean = false;
     filters: string;
-    imageSource: string;
 
     constructor(hostdiv: string){
         this.hostDiv = hostdiv;
@@ -15,7 +14,7 @@ class ImageUploader {
         <p><label for="file">Image</label></p>
         <p><input type="file" id="file" accept="image/*" name="image"></p><br/>
         <img id="output" width="600"><br/>
-        <a class="disabled" id="saveImage">Save Image</a>`;
+        <a class="disabled" id="saveImage" download>Save Image</a>`;
         imageUploaderContainer.classList.add('uploader');
         imageUploaderContainer.innerHTML = uploaderForm;
         hostContainer.appendChild(imageUploaderContainer);
@@ -34,7 +33,6 @@ class ImageUploader {
             this.imageFilters();
             btn.classList.remove("disabled");
         }
-        console.log(this.imageSource);
     }
 
     imageFilters(){
@@ -42,11 +40,17 @@ class ImageUploader {
         const filterContainer = document.createElement('div');
         const filters = `
         <label for="blur">Blur</label>
-        <input type="range" min="0" max="20" value="0"  id="blur">
+        <input type="range" min="0" max="20" value="0"  id="blur"><br>
         <label for="grayscale">GrayScale</label>
-        <input type="range" min="0" max="100" value="0" id="grayscale">
+        <input type="range" min="0" max="100" value="0" id="grayscale"><br>
         <label for="contrast">Contrast</label>
-        <input type="range" min="0" max="100" value="100" id="contrast">
+        <input type="range" min="0" max="200" value="100" id="contrast"><br>
+        <label for="sepia">Sepia</label>
+        <input type="range" min="0" max="100" value="0" id="sepia"><br>
+        <label for="opacity">Opacity</label>
+        <input type="range" min="0" max="100" value="100" id="opacity"><br>
+        <label for="saturate">Saturate</label>
+        <input type="range" min="0" max="100" value="100" id="saturate"><br>
         `;
         filterContainer.classList.add('filter-container');
         filterContainer.innerHTML = filters;
@@ -57,6 +61,12 @@ class ImageUploader {
         grayscale.addEventListener("change", this.updateCSSFilters);
         const contrast = document.getElementById("contrast");
         contrast.addEventListener("change", this.updateCSSFilters);
+        const sepia = document.getElementById("sepia");
+        sepia.addEventListener("change", this.updateCSSFilters);
+        const opacity = document.getElementById("opacity");
+        opacity.addEventListener("change", this.updateCSSFilters);
+        const saturate = document.getElementById("saturate");
+        saturate.addEventListener("change", this.updateCSSFilters);
     }
 
     updateCSSFilters(){      
@@ -64,10 +74,16 @@ class ImageUploader {
         let blurValue = document.getElementById("blur") as HTMLInputElement;
         let grayscaleValue = document.getElementById("grayscale") as HTMLInputElement;
         let contrastValue = document.getElementById("contrast") as HTMLInputElement;
+        let sepiaValue = document.getElementById("sepia") as HTMLInputElement;
+        let opacityValue = document.getElementById("opacity") as HTMLInputElement;
+        let saturateValue = document.getElementById("saturate") as HTMLInputElement;
         this.filters = `
         blur(${blurValue.value}px)
         grayscale(${grayscaleValue.value}%)
         contrast(${contrastValue.value}%)
+        sepia(${sepiaValue.value}%)
+        opacity(${opacityValue.value}%)
+        saturate(${saturateValue.value}%)
     `;  
     image.style.filter = this.filters;      
     }
@@ -83,10 +99,9 @@ class ImageUploader {
         canvas.height = 600;
         const ctx = canvas.getContext("2d");
         ctx.filter = tempImage['imageSource'].style.filter;
-        console.log(tempImage['imageSource'].style.filter);
+        console.log(tempImage['imageSource'].style.filter); //delete eventually
         ctx.drawImage(tempImage['imageSource'], 0, 0, canvas.width, canvas.height);
         downloadLink.href = canvas.toDataURL();
-        downloadLink.download = "image.png";
         main.appendChild(canvas);
 
     }
