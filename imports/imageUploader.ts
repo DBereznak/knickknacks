58a9b775142366ec7bd4e2187e85d6699cec22f8
DBereznak <dbereznak@gmail.com>
2020-11-18 01:07:02 -0500

    download image working

--- imports/imageUploader.ts
+++ imports/imageUploader.ts
@@ -1,6 +1,8 @@
 class ImageUploader {
     hostDiv: string;
     hasImage: boolean = false;
+    filters: string;
+    imageSource: string;
 
     constructor(hostdiv: string){
         this.hostDiv = hostdiv;
@@ -12,21 +14,27 @@ class ImageUploader {
         const uploaderForm: string = `
         <p><label for="file">Image</label></p>
         <p><input type="file" id="file" accept="image/*" name="image"></p><br/>
-        <img id="output" width="600">`;
+        <img id="output" width="600"><br/>
+        <a class="disabled" id="saveImage">Save Image</a>`;
         imageUploaderContainer.classList.add('uploader');
         imageUploaderContainer.innerHTML = uploaderForm;
         hostContainer.appendChild(imageUploaderContainer);
         const display = document.getElementById("file");
         display.addEventListener("change", event => this.displayImage(event));
+        const btn = document.getElementById("saveImage");
+        btn.addEventListener("click", this.downloadImage);
     }
 
     displayImage(event){
         const image = document.getElementById("output") as HTMLImageElement;
+        const btn = document.getElementById("saveImage") as HTMLElement;
         image.src = URL.createObjectURL(event.target.files[0]);
         this.hasImage = true;
         if(this.hasImage){
             this.imageFilters();
+            btn.classList.remove("disabled");
         }
+        console.log(this.imageSource);
     }
 
     imageFilters(){
@@ -56,13 +64,33 @@ class ImageUploader {
         let blurValue = document.getElementById("blur") as HTMLInputElement;
         let grayscaleValue = document.getElementById("grayscale") as HTMLInputElement;
         let contrastValue = document.getElementById("contrast") as HTMLInputElement;
-        console.log(blurValue.value + " : " + grayscaleValue.value + " : " + contrastValue.value);
-        image.style.filter = `
-            blur(${blurValue.value}px)
-            grayscale(${grayscaleValue.value}%)
-            contrast(${contrastValue.value}%)
-        `;        
+        this.filters = `
+        blur(${blurValue.value}px)
+        grayscale(${grayscaleValue.value}%)
+        contrast(${contrastValue.value}%)
+    `;  
+    image.style.filter = this.filters;      
+    }
+
+    downloadImage(){
+        const main = document.getElementById("main");
+        const canvas = document.createElement("canvas");
+        const downloadLink = document.getElementById("saveImage") as HTMLLinkElement;
+        let tempImage = {
+            imageSource:  document.getElementById("output") as HTMLImageElement
+        }
+        canvas.width = 600;
+        canvas.height = 600;
+        const ctx = canvas.getContext("2d");
+        ctx.filter = tempImage['imageSource'].style.filter;
+        console.log(tempImage['imageSource'].style.filter);
+        ctx.drawImage(tempImage['imageSource'], 0, 0, canvas.width, canvas.height);
+        downloadLink.href = canvas.toDataURL();
+        downloadLink.download = "image.png";
+        main.appendChild(canvas);
+
     }
 }
+
 
 export {ImageUploader};
